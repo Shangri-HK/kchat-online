dd5ccbb623686ba63e705332aab7dd681a0cac15
LAVOIX - MESBAH Khaled <MESBAH@domlavoix.fr>
2016-12-23 17:07:15 +0100

    private chat 1.0

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ avatars = [];
 colors = [];
 glows = [];
 connections = [];
+html = [];
+pcid = [];
+usersocket = [];
+var id = 0;
 //transports: ['polling']
 app.use(express.static(__dirname + '/public'));
 server.listen(process.env.PORT ||3000);
@@ -28,6 +32,7 @@ io.sockets.on('connection', function(socket){
 			colors.splice(users.indexOf(socket.username), 1);
 			glows.splice(users.indexOf(socket.username), 1);
 			users.splice(users.indexOf(socket.username), 1);
+			usersocket.splice(usersocket.indexOf(socket), 1);
 			io.sockets.emit('new message', {msg: data, user: socket.username, avatar: socket.avatar, alert: 2});
 			updateUsernames();
 			connections.splice(connections.indexOf(socket), 1);
@@ -49,7 +54,13 @@ io.sockets.on('connection', function(socket){
 	 {
 	 	var user = users.indexOf(name);
 
-	 	io.sockets.emit('get user info', {name: name, avatar: avatars[user], color: colors[users], glow: glows[user]});
+	 	pc = [];
+		pc.push(name);
+		pc.push(socket.username);
+		pc.sort();
+		id = pc[0] + pc[1];
+
+	 	io.sockets.emit('get user info', {name: name, avatar: avatars[user], color: colors[user], glow: glows[user], html: html[pcid.indexOf(id)]});
 	 });
 
 
@@ -67,6 +78,28 @@ io.sockets.on('connection', function(socket){
 		}
 	});
 
+	socket.on('join', function (data) {
+		
+		pc = [];
+		pc.push(data.user);
+		pc.push(socket.username);
+		pc.sort();
+		id = pc[0] + pc[1];
+		if (pcid.indexOf(id) == -1)
+		{
+			html.push('<li class="list-group-item affichage color"><img src="' + socket.avatar + '" id="avatar" class="color"/><span style="color:'  + ';'  + '">'  + '</span><br /><strong>' + data.msg  +'</strong></li>');
+			pcid.push(id);
+		}
+		else
+			html[pcid.indexOf(id)] = html[pcid.indexOf(id)] + '<li class="list-group-item affichage color"><img src="' + socket.avatar + '" id="avatar" class="color"/><span style="color:'  + ';'  + '">'  + '</span><br /><strong>' + data.msg  +'</strong></li>';
+    	socket.join(id);
+    	usersocket[users.indexOf(data.user)].emit('alert', {sender: socket.username});
+    	io.sockets.in(id).emit('private message', {msg: data.msg, user: socket.username, avatar: socket.avatar, color: socket.color, glow: socket.glow, id: id, html: html[pcid.indexOf(id)]});
+    	console.log(id);
+    	 // We are using room of socket io
+  	});
+
+
 	//New User
 	socket.on('new user', function(data, avatar, color, glow, add, callback){
 		callback(true);
@@ -80,6 +113,7 @@ io.sockets.on('connection', function(socket){
 		colors.push(socket.color);
 		glows.push(socket.glow);
 		users.push(socket.username);
+		usersocket.push(socket);
 		}
 		updateUsernames();
 	});
