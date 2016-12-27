f410c4a7ee37e1859d9c1fe92bca088e1ca5900e
Shangri-HK <aloea.heroiin@gmail.com>
2016-12-27 22:06:03 +0100

    test register

--- public/main.js
+++ public/main.js
@@ -3,5 +3,198 @@
  */
 
 $(document).ready(function() {
-    
+    var socket = io.connect();
+    var audio = new Audio('notif.mp3');
+    var $messageForm = $('#messageForm');
+    var $privateMessageForm = $('#privateMessageForm');
+    var $message = $('#message');
+    var $privateMessage = $('#privateMessage');
+    var $chat = $('#chat');
+    var $privateChat = $('#privateChat');
+    var $userForm = $('#userForm');
+    var $messageArea = $('#messageArea');
+    var $userFormArea = $('#userFormArea');
+    var $users = $('#users');
+    var $username = $('#username');
+    var $settings = $('#settings');
+    var $config = $('#config');
+    var $avatarLink = $('#avatarLink');
+    var $image = $('#image');
+    var $pseudo = $('#pseudo');;
+    var $prevImage;
+    var $prevPseudo = '';
+    var $cinema = $('#cinema');
+    var $youtubeLink = $('#youtubeLink');
+    var $background = $('#background');
+    var $colorTheme = $('#colorTheme');
+    var $usernameColor = $('#usernameColor');
+    var $usernameGlow = $('#usernameGlow');
+    var isCinema = false;
+    var youtubeId;
+    var logName;
+    var usernameInfo;
+    var avatarInfo;
+    var colorInfo;
+    var glowInfo;
+    var html;
+
+
+    $messageForm.submit(function(e){
+        e.preventDefault();
+        socket.emit('send message', $message.val(), null, null, null, true);
+        socket.emit('writing', logName, true);
+        $message.val('');
+    });
+
+    $privateMessageForm.submit(function(e){
+        e.preventDefault();
+        var privateText = $privateMessage.val();
+
+        privateText = isYoutube(privateText);
+        privateText = isImg(privateText);
+        privateText = emojis(privateText);
+        privateText = linkify(privateText);
+        socket.emit('join', {user: usernameInfo, msg: privateText});
+        socket.emit('writing', logName, true);
+        $privateMessage.val('');
+    });
+
+    socket.on('alert', function(data){
+        document.getElementById(data.sender).style.background = "white";
+        document.getElementById(data.sender).style.borderRadius = "0 10px 0 20px";
+    });
+
+    socket.on("private message", function(data) {
+        $('#privateChat').html(data.html);
+        x.scrollTop = x.scrollHeight;
+        audio.volume = 0.2;
+
+        alert(readCookie("sound"));
+        //$('#privateChat').append('<li class="list-group-item affichage color"><img src="' + avatarInfo + '" id="avatar" class="color"/><span style="color:'  + ';'  + '">'  + '</span><br /><strong>' + data.msg  +'</strong></li>');
+    });
+
+    socket.on('is writing', function(data)
+    {
+        if (data.stop == false)
+            document.getElementById(data.name).innerHTML = data.name + '<strong> ✎...</strong>';
+        else
+            document.getElementById(data.name).innerHTML = data.name;
+    });
+
+
+    //Message emit on chat or on user's connection/deconnection
+    socket.on('new message', function(data){
+        if (data.alert == 0){
+            var text = linkify(escapeHtml(data.msg));
+            var today = new Date();
+
+            text = isYoutube(text);
+            text = isImg(text);
+            text = emojis(text);
+            if (readCookie("textStyle") == "bold")
+                $chat.append('<li class="list-group-item affichage color"><img src="' + escapeHtml(data.avatar) + '" id="avatar" class="color"/><span style="color:' + data.color + ';' + returnGlow(data.glow) + '">' + escapeHtml(data.user) + '   ' /*+ today*/ + '</span><span id="time"></span><br /><strong>' + text  +'</strong></li>');
+            else
+                $chat.append('<li class="list-group-item affichage color"><img src="' + escapeHtml(data.avatar) + '" id="avatar" class="color"/><strong style="color:' + data.color + ';' + returnGlow(data.glow) + '">' + escapeHtml(data.user) + '   ' /*+ today*/ + '</strong><br />' + text  +'</li>');
+        }
+        else if (data.alert == 1)
+            $chat.append('<li class="list-group-item affichage" style="background-color: grey; height: 20px;font-size: 10px; overflow:hidden; padding-top: 2px;"><strong>' + data.user + ' is connected.<strong/>')
+        else
+            $chat.append('<li class="list-group-item affichage" style="background-color: grey; height: 20px;font-size: 10px; overflow:hidden; padding-top: 2px;"><strong>' + data.user + ' has disconnected.<strong/>')
+        var x = document.getElementById('chat');
+        x.scrollTop = x.scrollHeight;
+        audio.volume = 0.2;
+        if (readCookie("sound") == "Y")
+            audio.play();
+    });
+
+
+    //Add the user's informations at login
+    $("#login").click(function(e){
+        e.preventDefault();
+        socket.emit('new user', $username.val(), $('#password').val(), $avatarLink.val(), readCookie("usernameColor"), readCookie("usernameGlow"), true, function(data){
+            console.log(data);
+            if(data == true)
+            {
+                $userFormArea.hide();
+                $messageArea.fadeIn();
+                createCookie("username", $username.val());
+                createCookie("avatar", $avatarLink.val());
+                logName = $username.val();
+                $('#cinemaOn').fadeIn();
+                changeColor(readCookie("colorTheme"));
+
+            } else {
+                $('#errorLogin').show();
+            }
+        });
+        if (typeof readCookie("background") != "undefined")
+            document.getElementById('body').style.backgroundImage = "url(background.jpg)";
+        else
+            document.getElementById('body').style.backgroundImage = "url(" + readCookie("background") + ")";
+        document.getElementById('pseudo').value = $username.val();
+        //$username.val('');
+    });
+
+    //Show the users online on the sidebar
+    socket.on('get users', function(data){
+        var html = '';
+        for(i = 0; i < data.length; i++)
+        {
+            html += '<li class="onlineUser" id="' + data[i] + '" onclick="userInfo(this.id, '+data[i]+')">' + escapeHtml(data[i]) + '</li>';
+        }
+        $users.html(html);
+    });
+
+    $('#registerButton').click(function() {
+        $('#registerFormArea').show();
+        $('#userFormArea').hide();
+    });
+
+    //Register new user
+    $("#register").click(function(e) {
+        e.preventDefault();
+
+        var registerUsername = $('#registerUsername').val();
+        var registerPassword = $('#registerPass').val();
+        var registerAvatar = $('#registerAvatar').val();
+
+        //register user
+        socket.emit('register user', registerUsername, registerPassword, registerAvatar, function(isRegistered) {
+            console.log("registered:" + isRegistered);
+
+            if (isRegistered == true) {
+                $('#registerFormArea').hide();
+
+                //then connect as normal user
+                socket.emit('new user', registerUsername, registerPassword, registerAvatar, "", "", true, function(data){
+                    if(data == true)
+                    {
+                        //console.log(data);
+                        $userFormArea.hide();
+                        $messageArea.fadeIn();
+                        createCookie("username", registerUsername);
+                        createCookie("avatar", registerAvatar);
+                        logName = registerUsername.val();
+                        $('#cinemaOn').fadeIn();
+                        changeColor(readCookie("colorTheme"));
+
+                    } else {
+                        $('#errorLogin').show();
+                    }
+                });
+
+            } else {
+                $('#errorRegister').show();
+            }
+        });
+
+
+
+
+        if (typeof readCookie("background") != "undefined")
+            document.getElementById('body').style.backgroundImage = "background.jpg";
+        else
+            document.getElementById('body').style.backgroundImage = "url(" + readCookie("background") + ")";
+        document.getElementById('pseudo').value = $username.val();
+    });
 });
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var mysql = require('mysql');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -15,6 +16,18 @@ var id = 0;
 app.use(express.static(__dirname + '/public'));
 server.listen(process.env.PORT ||3000);
 console.log('Server running...');
+
+//Mysql Connection
+var connection = mysql.createConnection({
+	host: 'thehiddenride420.mysql.db',
+	user: 'thehiddenride420',
+	password: 'Uskxkqbl4862',
+	database: 'thehiddenride420'
+});
+
+connection.connect();
+
+//End Mysql Connection
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -38,30 +51,30 @@ io.sockets.on('connection', function(socket){
 			connections.splice(connections.indexOf(socket), 1);
 			console.log('Disconnected: %s sockets connected', connections.length);
 		}
-		
+
 	});
 
 	//socket.on('close', function(){
 	//this.transport.close();
-	 //});
+	//});
 
-	 socket.on('writing', function(name, stop)
-	 {
-	 	io.sockets.emit('is writing', {name: socket.username, stop: stop});
-	 });
+	socket.on('writing', function(name, stop)
+	{
+		io.sockets.emit('is writing', {name: socket.username, stop: stop});
+	});
 
-	 socket.on('user info', function(name, avatar, color, glow)
-	 {
-	 	var user = users.indexOf(name);
+	socket.on('user info', function(name, avatar, color, glow)
+	{
+		var user = users.indexOf(name);
 
-	 	pc = [];
+		pc = [];
 		pc.push(name);
 		pc.push(socket.username);
 		pc.sort();
 		id = pc[0] + pc[1];
 
-	 	io.sockets.emit('get user info', {name: name, avatar: avatars[user], color: colors[user], glow: glows[user], html: html[pcid.indexOf(id)]});
-	 });
+		io.sockets.emit('get user info', {name: name, avatar: avatars[user], color: colors[user], glow: glows[user], html: html[pcid.indexOf(id)]});
+	});
 
 
 	//Send Message
@@ -79,7 +92,7 @@ io.sockets.on('connection', function(socket){
 	});
 
 	socket.on('join', function (data) {
-		
+
 		pc = [];
 		pc.push(data.user);
 		pc.push(socket.username);
@@ -92,36 +105,112 @@ io.sockets.on('connection', function(socket){
 		}
 		else
 			html[pcid.indexOf(id)] = html[pcid.indexOf(id)] + '<li class="list-group-item affichage color"><img src="' + socket.avatar + '" id="avatar" class="color"/><span style="color:' + socket.color + ';' + returnGlow(socket.glow) + '">' + socket.username + '</span><br /><strong>' + data.msg  +'</strong></li>';
-    	socket.join(id);
-    	usersocket[users.indexOf(data.user)].emit('alert', {sender: socket.username});
-    	io.sockets.in(id).emit('private message', {msg: data.msg, user: socket.username, avatar: socket.avatar, color: socket.color, glow: socket.glow, id: id, html: html[pcid.indexOf(id)]});
-    	console.log(id);
-    	 // We are using room of socket io
-  	});
+		socket.join(id);
+		usersocket[users.indexOf(data.user)].emit('alert', {sender: socket.username});
+		io.sockets.in(id).emit('private message', {msg: data.msg, user: socket.username, avatar: socket.avatar, color: socket.color, glow: socket.glow, id: id, html: html[pcid.indexOf(id)]});
+		console.log(id);
+		// We are using room of socket io
+	});
 
 
 	//New User
-	socket.on('new user', function(data, avatar, color, glow, add, callback){
-		callback(true);
+	socket.on('new user', function(data, password, avatar, color, glow, add, callback){
+
+		//mysql query login
+		connection.query('SELECT * FROM kchat_users WHERE username = "' + data+ '" AND user_password = "' + password + '"', function (err, rows, fields) {
+			if (err) { //if there is an error in the mysql query
+				callback(false);
+			} else {
+				if (rows.length != 0) {
+					//if username & password are good, callback true, user connected
+					if (rows[0]['username'] == data && rows[0]['user_password'] == password) {
+						console.log('user connected as : ', rows[0]['username']);
+						callback(true);
+					} else {
+						//if wrong credentials (usually, this condition will not be used)
+						console.error("user failed to login with credentials: " + data + " : " + password);
+						callback(false);
+					}
+				} else {
+					//if wrong credentials (0 db return)
+					console.error("user failed to login with credentials: " + data + " : " + password);
+				}
+			}
+		});
+
+
 		socket.username = data;
 		socket.avatar = avatar;
 		socket.color = color;
 		socket.glow = glow;
 		if(add){
 			io.sockets.emit('new message', {msg: data, user: socket.username, avatar: socket.avatar, color: socket.color, glow: socket.glow, alert: 1});
-		avatars.push(socket.avatar);
-		colors.push(socket.color);
-		glows.push(socket.glow);
-		users.push(socket.username);
-		usersocket.push(socket);
+			avatars.push(socket.avatar);
+			colors.push(socket.color);
+			glows.push(socket.glow);
+			users.push(socket.username);
+			usersocket.push(socket);
 		}
 		updateUsernames();
 	});
 
+	//new register
+	socket.on('register user', function(username, password, avatar, callback) {
+		var  registered = false;
+		//check if username if not taken
+		connection.query('SELECT * from kchat_users WHERE username = "'+username+'"', function(err, rows, fields){
+			if (err) {
+				console.log('error here');
+				callback(false);
+				registered = false;
+			} else {
+				if (rows.length != 0) {
+					console.log("rows"+rows);
+					callback(false);
+					registered = false;
+				} else {
+					console.log('here: ' + registered);
+					callback(true);
+					registered = true;
+				}
+			}
+
+			console.log(registered);
+
+			if (registered == true) {
+				connection.query('INSERT INTO kchat_users VALUES("", "'+username+'", "'+password+'")', function(err) {
+					if (err) {
+						callback(false);
+						console.error(err);
+					} else {
+
+						console.log('succes');
+					}
+
+				});
+			}
+
+			socket.username = username;
+			socket.avatar = avatar;
+			socket.glow = "";
+			socket.color = "";
+
+			io.sockets.emit('new message', {msg: username, user: socket.username, avatar: socket.avatar, color: socket.color, glow: socket.glow, alert: 1});
+			avatars.push(socket.avatar);
+			colors.push(socket.color);
+			glows.push(socket.glow);
+			users.push(socket.username);
+			usersocket.push(socket);
+			updateUsernames();
+		});
+		});
+
+
+
 	function returnGlow(data)
 	{
 		if (data != "")
-			return "text-shadow: 0px 0px 5px" +  data + ";"
+			return "text-shadow: 0px 0px 5px" +  data + ";";
 		else
 			return "";
 	}
@@ -129,4 +218,5 @@ io.sockets.on('connection', function(socket){
 	function updateUsernames(){
 		io.sockets.emit('get users', users);
 	}
+
 });
